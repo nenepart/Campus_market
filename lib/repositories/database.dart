414e38a235832374f655d8455e5eeec823881a16
Michael Ameteku <michaelameteku@gmail.com>
2023-04-15 13:39:24 -0400

    Fix errors

--- lib/repositories/database.dart
+++ lib/repositories/database.dart
@@ -1,25 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DatabaseServices{
+class DatabaseService {
+  final FirebaseFirestore _firestoreInstance;
 
-  final String uid;
-  DatabaseServices({ required this.uid });
+  DatabaseService() : _firestoreInstance = FirebaseFirestore.instance;
 
-  //collection reference
-  final CollectionReference studentCollection =
-      FirebaseFirestore.instance.collection('Students');
+  Future<List<QueryDocumentSnapshot>> getCollection(String path) {
+    return _firestoreInstance.collection(path).get().then(
+          (value) => value.docs,
+        );
+  }
 
-  Future updateUserData(String firstName, String lastName, String college,
-      String location) async {
-    return await studentCollection.doc(uid).set({
-      'first name': firstName,
-      'last name': lastName,
-      'campus': college,
-      'location': location,
+  Future<DocumentSnapshot<Map<String, dynamic>>?> getDocument(String path) {
+    try {
+      return _firestoreInstance.doc(path).get();
+    } catch (exception) {
+      print("document does not exist for path: $path error: $exception");
+      return Future.value(null);
+    }
+  }
+
+  Future<String?> createDocument(String collectionPath, Map<String, dynamic> json, {String? docId}) {
+    DocumentReference docRef = _firestoreInstance.collection(collectionPath).doc(docId);
+    return docRef.set(json).then((value) => docRef.id, onError: (error) {
+      print("An error occured: ${error}");
+      return "";
     });
   }
-  //get student stream
-Stream<QuerySnapshot> get students {
-    return studentCollection.snapshots();
-}
+
+  Future<bool> deleteDocument(String docPath) {
+    return _firestoreInstance.doc(docPath).delete().then((value) => true).onError((error, stackTrace) {
+      return false;
+    });
+  }
+
+  Future<bool> updateDocument(String documentPath, Map<String, dynamic> data) =>
+      _firestoreInstance.doc(documentPath).update(data).then((value) => true).onError((error, stackTrace) {
+        return false;
+      });
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getCollectionStream(String collectionPath) {
+    return _firestoreInstance.collection(collectionPath).snapshots();
+  }
+
+  CollectionReference<Map<String, dynamic>> getCollectionReference(String collectionPath) {
+    return _firestoreInstance.collection(collectionPath);
+  }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,22 +1,58 @@
+import 'dart:developer';
+
+import 'package:campus_market/models/user_model.dart';
+import 'package:campus_market/repositories/user_repo.dart';
 import 'package:campus_market/screens/home_screen.dart';
 import 'package:campus_market/screens/signin_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:campus_market/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-class Wrapper extends StatelessWidget {
-  const Wrapper({Key? key}) : super(key: key);
+class Wrapper extends StatefulWidget {
+  const Wrapper({Key? key, required this.userRepo}) : super(key: key);
+
+  final UserRepo userRepo;
+
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  bool showSignIn = true;
+
+  @override
+  void initState() {
+    widget.userRepo.firestoreUserStream.addListener(() {
+      log("change!!! ");
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final user = Provider.of<User?>(context);
-
-    //return either home signin
-    if (user == null) {
-      return SignInScreen();
-    }else{
-      return HomeScreen();
-    }
+    log("User repo: ${widget.userRepo.firestoreUserStream}");
+    return ValueListenableBuilder(
+      valueListenable: widget.userRepo.firestoreUserStream,
+      builder: (BuildContext context, UserModel? value, Widget? child) {
+        log("In Wrapper $value");
+        //return either home signin
+        if (value == null) {
+          if (showSignIn) {
+            return SignInScreen(goToSignUp: () {
+              setState(() {
+                showSignIn = false;
+              });
+            });
+          } else {
+            return SignUpScreen(goToSignUp: () {
+              setState(() {
+                showSignIn = true;
+              });
+            });
+          }
+        } else {
+          return const HomeScreen();
+        }
+      },
+    );
   }
 }
